0e02b54ad7f70efa54b480274490dc8cec0349c5
AmitKumarn <121242507+AmitKumarn@users.noreply.github.com>
2023-08-28 21:46:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ io.on('connection', (socket) => {
 
 
 // Start the server
-server.listen(port, () => {
+server.listen(process.env.PORT || 5000 ,() => {
   console.log(`Server started on port ${port}`);
 });
 
