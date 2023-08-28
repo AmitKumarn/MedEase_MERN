f066b2e89050325d2c0ed359af24606e80730f71
AmitKumarn <121242507+AmitKumarn@users.noreply.github.com>
2023-08-28 22:31:55 +0530

    Update Chat.js

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -16,7 +16,7 @@ const Chat = () => {
 
   const fetchChatMessages = useCallback(async () => {
     try {
-      const response = await fetch(`${BASE_UR}/api/chat/${sessionId}`);
+      const response = await fetch(`${BASE_URL}/api/chat/${sessionId}`);
       const data = await response.json();
       setMessages(data.chatMessages);
     } catch (error) {
@@ -41,7 +41,7 @@ const Chat = () => {
   const handleSendMessage = async () => {
     if (inputMessage.trim() !== '') {
       try {
-        await fetch(`${BASE_UR}/api/chat/send`, {
+        await fetch(`${BASE_URL}/api/chat/send`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
